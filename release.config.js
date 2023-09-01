45d0d526697798c609af7ae211f8abed201d2c8a
miguelpuiggarcia <miguelpg@nandos.co.uk>
2023-09-01 10:55:55 +0100

    feat: implement semantic ver

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: ["main"], // You can extend this as required
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+  ],
+};
