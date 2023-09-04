076366496dbecd5b076299f7b0fcd4776a0f043d
miguelpuiggarcia <miguelpg@nandos.co.uk>
2023-09-04 13:13:09 +0100

    feat: add api gateway

--- gcp/cloud-function-v1/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "function_name" {
-  description = "The name of the deployed function."
-  value       = google_cloudfunctions_function.function.name
-}
-
-output "function_url" {
-  description = "The URL of the deployed function."
-  value       = google_cloudfunctions_function.function.https_trigger_url
-}
