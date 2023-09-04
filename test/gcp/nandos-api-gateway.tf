2be52c787b948f8729eee53329438a0162f2005e
miguelpuiggarcia <miguelpg@nandos.co.uk>
2023-09-04 10:22:46 +0100

    feat: add api gateway

--- test/gcp/nandos-api-gateway.tf
+++ test/gcp/nandos-api-gateway.tf
@@ -4,13 +4,4 @@ module "nando-api-gateway-api-1" {
   api_name               = "test-api"
   openapi_spec_file_path = "../assets/api-gateway-example.yml"
   project_region         = "europe-west2"
-  services = [
-    {
-      name = module.cloud-run-api-my-awesome-api.service_name,
-      url  = module.cloud-run-api-my-awesome-api.service_url
-    }
-  ]
-  depends_on = [
-    module.cloud-run-api-my-awesome-api
-  ]
 }
