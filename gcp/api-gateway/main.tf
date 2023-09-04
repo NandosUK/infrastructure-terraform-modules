2bd97d072d3c50afd6e7b0b4b017b2253dea7f3a
miguelpuiggarcia <miguelpg@nandos.co.uk>
2023-09-04 16:44:24 +0100

    feat: add api gateway

--- gcp/api-gateway/main.tf
+++ gcp/api-gateway/main.tf
@@ -60,6 +60,34 @@ resource "google_api_gateway_gateway" "nandos_api_gateway" {
   region     = var.project_region
 }
 
+# Enable the api so we can generate keys against it
+resource "google_project_service" "enable_api_gateway" {
+  service = google_api_gateway_api.nandos_api.managed_service
+
+  disable_on_destroy = false # Set to 'true' if you want to disable the service when destroying this Terraform resource
+}
+
+
+resource "google_apikeys_key" "primary" {
+  name         = "key-my-test-123"
+  display_name = "Key for XXX 123"
+  project      = var.project_id
+
+  restrictions {
+    api_targets {
+      service = google_api_gateway_api.nandos_api.managed_service
+      methods = ["GET*"]
+    }
+
+    server_key_restrictions {
+      allowed_ips = ["127.0.0.1"]
+    }
+  }
+  depends_on = [
+    google_project_service.enable_api_gateway
+  ]
+}
+
 
 resource "google_compute_region_network_endpoint_group" "api_g_neg" {
   provider              = google-beta
