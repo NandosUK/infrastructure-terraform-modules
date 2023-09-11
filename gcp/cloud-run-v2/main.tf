5bc96f95d6c02f6587a9dcaa1f7da8f0ed49356d
miguelpuiggarcia <miguelpg@nandos.co.uk>
2023-09-11 12:45:58 +0100

    Support trigger eventarc

--- gcp/cloud-run-v2/main.tf
+++ gcp/cloud-run-v2/main.tf
@@ -200,13 +200,12 @@ module "lb-http" {
   }
 }
 
-/* 
+
 resource "google_eventarc_trigger" "default" {
-  provider                = google-beta
-  for_each                = { for i, trigger in var.eventarc_triggers : i => trigger }
-  event_data_content_type = "application/protobuf"
-  name                    = "trigger-${google_cloud_run_v2_service.default.name}"
-  location                = var.project_region
+  for_each = { for i, trigger in var.eventarc_triggers : i => trigger }
+
+  name     = "trigger-${google_cloud_run_v2_service.default.name}"
+  location = var.project_region
   matching_criteria {
     attribute = "type"
     value     = each.value.event_type
@@ -217,30 +216,6 @@ resource "google_eventarc_trigger" "default" {
     cloud_run_service {
       service = google_cloud_run_v2_service.default.name
       path    = each.value.api_path
-      region  = var.project_region
-    }
-  }
-} */
-
-
-resource "google_eventarc_trigger" "trigger-auditlog-tf" {
-  name     = "trigger-auditlog-tf"
-  location = var.project_region
-  matching_criteria {
-    attribute = "type"
-    value     = "google.cloud.audit.log.v1.written"
-  }
-  matching_criteria {
-    attribute = "serviceName"
-    value     = "storage.googleapis.com"
-  }
-  matching_criteria {
-    attribute = "methodName"
-    value     = "storage.objects.create"
-  }
-  destination {
-    cloud_run_service {
-      service = google_cloud_run_v2_service.default.name
       region  = var.project_region
     }
   }
