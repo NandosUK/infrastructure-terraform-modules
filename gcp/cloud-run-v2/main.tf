526da1c204a50ca975900672a0751c196742cb5f
miguelpuiggarcia <miguelpg@nandos.co.uk>
2023-09-11 12:42:27 +0100

    Support trigger eventarc

--- gcp/cloud-run-v2/main.tf
+++ gcp/cloud-run-v2/main.tf
@@ -200,13 +200,7 @@ module "lb-http" {
   }
 }
 
-resource "google_storage_bucket" "testb123" {
-  name          = "test-bucket-miguelpgtestaurora"
-  location      = "europe-west2"
-  force_destroy = true
-
-}
-
+/* 
 resource "google_eventarc_trigger" "default" {
   provider                = google-beta
   for_each                = { for i, trigger in var.eventarc_triggers : i => trigger }
@@ -218,16 +212,35 @@ resource "google_eventarc_trigger" "default" {
     value     = each.value.event_type
   }
 
-  matching_criteria {
-    attribute = "bucket"
-    value     = google_storage_bucket.testb123.name
-  }
-
   service_account = var.cloud_run_service_account
   destination {
     cloud_run_service {
       service = google_cloud_run_v2_service.default.name
       path    = each.value.api_path
+      region  = var.project_region
+    }
+  }
+} */
+
+
+resource "google_eventarc_trigger" "trigger-auditlog-tf" {
+  name     = "trigger-auditlog-tf"
+  location = var.project_region
+  matching_criteria {
+    attribute = "type"
+    value     = "google.cloud.audit.log.v1.written"
+  }
+  matching_criteria {
+    attribute = "serviceName"
+    value     = "storage.googleapis.com"
+  }
+  matching_criteria {
+    attribute = "methodName"
+    value     = "storage.objects.create"
+  }
+  destination {
+    cloud_run_service {
+      service = google_cloud_run_v2_service.default.name
       region  = var.project_region
     }
   }
