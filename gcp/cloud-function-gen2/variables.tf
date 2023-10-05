89868d70ebafc38b849e47d8d24e706b81b150ad
Aurora <aurora.delrioperez@nandos.co.uk>
2023-10-05 12:24:16 +0100

    types removed

--- gcp/cloud-function-gen2/variables.tf
+++ gcp/cloud-function-gen2/variables.tf
@@ -83,17 +83,17 @@ variable "event_type" {
 
 variable "event_trigger" {
   description = "List of event triggers"
-  type = {
-    trigger_region        = string
-    event_type            = string
-    retry_policy          = string
-    service_account_email = string
-    event_filters = list(object({
-      attribute       = string
-      value           = string
-      operator        = optional(string)
-    }))
-  }
+  # type = {
+  #   trigger_region        = string
+  #   event_type            = string
+  #   retry_policy          = string
+  #   service_account_email = string
+  #   event_filters = list(object({
+  #     attribute       = string
+  #     value           = string
+  #     operator        = optional(string)
+  #   }))
+  # }
   default     = null
 }
 
