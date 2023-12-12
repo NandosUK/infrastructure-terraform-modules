67d4e1661533b27b05e874a01ac75c66d761133e
Aurora Del Rio <aurora.delrioperez@nandos.co.uk>
2023-12-12 14:25:35 +0000

    default

--- gcp/cloud-run-v2/variables.tf
+++ gcp/cloud-run-v2/variables.tf
@@ -197,6 +197,7 @@ variable "location" {
  
 variable "trigger_substitutions" {
   description = "Substitution variable for the trigger, think about Buckets names, pubsub names, service accounts, etc. Anything dynamic you will need to deploy this function (via Yaml file)"
+  default = {}
 }
 
 variable "trigger_config" {
