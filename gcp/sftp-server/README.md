92173b89dbf1460f58233f66582e3c00cc7c47ff
Sam Royal <sam.royal@nandos.co.uk>
2023-12-19 05:34:24 +0000

    feat(sftp-server): the rest of it

--- gcp/sftp-server/README.md
+++ gcp/sftp-server/README.md
@@ -2,6 +2,14 @@
 
 This Terraform module allows you to create an SFTP server hosted in Google Cloud Platform (GCP) that stores files in a Google Cloud Storage bucket. It sets up the necessary infrastructure, including the VM instance, network, firewall rules, and more. It uses Thorntech's [SFTP Gateway](https://thorntech.com/sftp-gateway-for-google-cloud/).
 
+## Prerequisites
+
+Ensure the following APIs are enabled in your GCP project (the last two might not be necessary as we're managing through terraform, but I haven't tested without):
+
+- `compute.googleapis.com`
+- `deploymentmanager.googleapis.com`
+- `runtimeconfig.googleapis.com`
+
 ## Accessing the SFTP Server
 
 You can access the SFTP server using an SFTP client, such as [FileZilla](https://filezilla-project.org/) or the `sftp` command in your terminal. Use the following information:
--- gcp/sftp-server/main.tf
+++ gcp/sftp-server/main.tf
@@ -1,5 +1,12 @@
 locals {
-  tags = ["terraform-instance"]
+  image              = "thorn-technologies-public/sftpgw-3-4-4-1694633054"
+  tags               = ["sftp-instance"]
+  nandos_internal_ip = "203.206.117.131"
+  # https://cloud.google.com/load-balancing/docs/health-check-concepts#ip-ranges
+  gcp_healthcheck_ip_1 = "130.211.0.0/22"
+  gcp_healthcheck_ip_2 = "35.191.0.0/16"
+  # https://cloud.google.com/iap/docs/using-tcp-forwarding
+  gcp_iap_ip = "35.235.240.0/20"
 }
 
 resource "google_storage_bucket" "bucket" {
@@ -13,51 +20,17 @@ resource "google_compute_network" "vpc_network" {
   project = var.project
 }
 
-resource "google_compute_global_address" "default" {
-  name    = "tcp-proxy-xlb-ip"
-  project = var.project
+resource "google_service_account" "default" {
+  project      = var.project
+  account_id   = "sftpuser"
+  display_name = "Custom SA for SFTP VM Instance"
 }
 
-resource "google_compute_target_tcp_proxy" "default" {
-  name            = "${var.name}-proxy"
-  project         = var.project
-  backend_service = google_compute_backend_service.default.id
-}
-
-resource "google_compute_global_forwarding_rule" "default" {
-  name                  = "${var.name}-fwd"
-  project               = var.project
-  ip_protocol           = "TCP"
-  load_balancing_scheme = "EXTERNAL"
-  port_range            = "22"
-  target                = google_compute_target_tcp_proxy.default.id
-  ip_address            = google_compute_global_address.default.id
-}
-
-resource "google_compute_backend_service" "default" {
-  name                  = "tcp-proxy-xlb-backend-service"
-  project               = var.project
-  protocol              = "TCP"
-  port_name             = "tcp-port-22"
-  load_balancing_scheme = "EXTERNAL"
-  timeout_sec           = 10
-  health_checks         = [google_compute_health_check.default.id]
-  backend {
-    group           = google_compute_instance_group.default.self_link
-    balancing_mode  = "UTILIZATION"
-    max_utilization = 1.0
-    capacity_scaler = 1.0
-  }
-}
-
-resource "google_compute_health_check" "default" {
-  name               = "${var.name}-health"
-  timeout_sec        = 1
-  check_interval_sec = 10
-  project            = var.project
-  tcp_health_check {
-    port = "80"
-  }
+resource "google_project_iam_member" "sftpuser" {
+  for_each = toset(["roles/compute.admin", "roles/storage.admin"])
+  project  = var.project
+  member   = google_service_account.default.member
+  role     = each.key
 }
 
 resource "google_compute_instance" "default" {
@@ -68,7 +41,7 @@ resource "google_compute_instance" "default" {
   tags         = local.tags
   boot_disk {
     initialize_params {
-      image = "thorn-technologies-public/sftpgw-3-4-4-1694633054"
+      image = local.image
     }
   }
   network_interface {
@@ -80,52 +53,105 @@ resource "google_compute_instance" "default" {
   shielded_instance_config {
     enable_secure_boot = true
   }
+  service_account {
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
 }
 
-resource "google_compute_instance_group" "default" {
-  name        = "${var.name}-ig"
-  project     = var.project
-  zone        = var.zone
-  description = "Instance group for all ports"
-  instances   = [google_compute_instance.default.self_link]
-  named_port {
-    name = "tcp-port-22"
-    port = 22
-  }
-  named_port {
-    name = "tcp-port-80"
-    port = 80
-  }
-  named_port {
-    name = "tcp-port-443"
-    port = 443
-  }
-  named_port {
-    name = "tcp-port-2222"
-    port = 2222
-  }
+resource "google_compute_global_address" "default" {
+  name    = "${var.name}-ip"
+  project = var.project
+}
+
+module "load-balancer-sftp" {
+  source                  = "../load-balancer"
+  project                 = var.project
+  zone                    = var.zone
+  name                    = "${var.name}-sftp"
+  port                    = 22
+  google_compute_instance = google_compute_instance.default.self_link
+  ip_address              = google_compute_global_address.default.address
+}
+
+module "load-balancer-http" {
+  source                  = "../load-balancer"
+  project                 = var.project
+  zone                    = var.zone
+  name                    = "${var.name}-http"
+  port                    = 80
+  google_compute_instance = google_compute_instance.default.self_link
+  ip_address              = google_compute_global_address.default.address
+}
+
+module "load-balancer-https" {
+  source                  = "../load-balancer"
+  project                 = var.project
+  zone                    = var.zone
+  name                    = "${var.name}-https"
+  port                    = 443
+  google_compute_instance = google_compute_instance.default.self_link
+  ip_address              = google_compute_global_address.default.address
+}
+
+# openssh access to the VM
+# but you probably don't need ssh access to the vm
+module "load-balancer-ssh" {
+  source                  = "../load-balancer"
+  project                 = var.project
+  zone                    = var.zone
+  name                    = "${var.name}-ssh"
+  port                    = 2222
+  google_compute_instance = google_compute_instance.default.self_link
+  ip_address              = google_compute_global_address.default.address
 }
 
 resource "google_compute_firewall" "default" {
-  name    = "${var.name}-fw"
-  project = var.project
-  network = google_compute_network.vpc_network.name
+  name        = "${var.name}-fw"
+  project     = var.project
+  network     = google_compute_network.vpc_network.name
+  target_tags = local.tags
+  direction   = "INGRESS"
   allow {
     protocol = "tcp"
-    ports    = ["80", "443", "2222"]
+    ports    = ["80", "443", "22", "2222"]
   }
-  target_tags   = local.tags
-  source_ranges = [google_compute_global_address.default.address]
+  source_ranges = [
+    local.nandos_internal_ip,
+    local.gcp_healthcheck_ip_1,
+    local.gcp_healthcheck_ip_2,
+    local.gcp_iap_ip,
+    google_compute_global_address.default.address,
+  ]
 }
 
-resource "google_compute_firewall" "sftp-port-22" {
-  name    = "${var.name}-sftp-fw"
-  project = var.project
-  network = google_compute_network.vpc_network.name
+resource "google_compute_firewall" "egress" {
+  name        = "${var.name}-egress-fw"
+  project     = var.project
+  network     = google_compute_network.vpc_network.name
+  direction   = "EGRESS"
+  priority    = 1000
+  target_tags = local.tags
   allow {
     protocol = "tcp"
-    ports    = ["22"]
+    ports    = ["80", "443"]
   }
-  target_tags   = local.tags
-  source_ranges = [google_compute_global_address.default.address]
+}
+
+# router/NAT to allow egress traffic from sftp server without external IP
+resource "google_compute_router" "default" {
+  project = var.project
+  name    = "${var.name}-router"
+  network = google_compute_network.vpc_network.name
+  region  = var.region
+}
+
+module "cloud-nat" {
+  source                             = "terraform-google-modules/cloud-nat/google"
+  version                            = "~> 5.0"
+  project_id                         = var.project
+  region                             = var.region
+  router                             = google_compute_router.default.name
+  name                               = "${var.name}-nat"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
