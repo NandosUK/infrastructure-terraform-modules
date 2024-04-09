2d851f0f4f4d1516bee51740ddef53cc45645033
Rafee Jenkins <rafee.jenkins@nandos.co.uk>
2024-04-09 15:51:05 +0100

    feat: add dummy storage trigger function

--- test/gcp/cloud-function-gen2.tf
+++ test/gcp/cloud-function-gen2.tf
@@ -16,13 +16,61 @@ module "cloud-function-my-awesome-cf" {
   repository_name       = "my-awesome-cf"
   project_id            = "mgt-build-56d2ff6b"
   timeout_seconds       = 60
+  event_trigger = {
+    trigger_region        = "europe-west2"
+    event_type            = "http"
+    retry_policy          = ""
+    service_account_email = "test-service-account-email"
+    event_filters = []
+  }
   trigger_substitutions = {
     _RUNTIME               = "nodejs16"
     _ENTRYPOINT            = "helloWorld"
     _FUNCTION_SA           = "test-service-account-email"
     _FUNCTION_PATH         = "services/my-awesome-cf/functions/my-awesome-cf"
     _FUNCTION_TYPE_TRIGGER = "trigger-http"
+  }
+  environment_variables = {
+    LOCATION = "europe-west2"
+  }
+  secret_keys          = []
+  threshold_value      = 0
+  function_entry_point = "helloWorld"
+}
 
+module "cloud-function-my-awesome-cf-storage" {
+  source                = "../../gcp/cloud-function-gen2"
+  function_name         = "my-awesome-cf"
+  function_type         = "node"
+  function_description  = "My awesome cloud function"
+  environment           = "preview"
+  region                = "europe-west2"
+  notification_channels = []
+  service_name          = "my-awesome-cf"
+  bucket_functions      = "test-bucket-functions"
+  service_account_email = "test-service-account-email"
+  function_path         = "services/my-awesome-cf/functions/my-awesome-cf"
+  max_instance_count    = 3
+  min_instance_count    = 1
+  branching_strategy    = "master"
+  repository_name       = "my-awesome-cf"
+  project_id            = "mgt-build-56d2ff6b"
+  timeout_seconds       = 60
+  event_type            = "STORAGE"
+  event_trigger = {
+    bucket_name           = "test-bucket"
+    trigger_region        = "europe-west2"
+    event_type            = "google.storage.object.finalize"
+    retry_policy          = ""
+    service_account_email = "test-service-account-email"
+    event_filters = []
+  }
+  trigger_substitutions = {
+    _RUNTIME               = "nodejs16"
+    _ENTRYPOINT            = "helloWorld"
+    _FUNCTION_SA           = "test-service-account-email"
+    _FUNCTION_PATH         = "services/my-awesome-cf/functions/my-awesome-cf"
+    _FUNCTION_TYPE_TRIGGER = "trigger-http"
   }
   environment_variables = {
     LOCATION = "europe-west2"
